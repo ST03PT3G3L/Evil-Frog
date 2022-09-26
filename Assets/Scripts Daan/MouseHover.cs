5edf115ff8d480106eef3186307d71050241b822
Daan <i464733@fhict.nl>
2022-09-26 13:25:11 +0200

    Simple grid system & tower placement

---
+++ Assets/Scripts Daan/MouseHover.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHover : MonoBehaviour
+{
+    public Transform currentlyHovering;
+
+    private void Update()
+    {
+        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector2(mouseWorldPosition.x, mouseWorldPosition.y);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform != null)
+        {
+            currentlyHovering = collision.transform;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if(currentlyHovering.tag == "Dragable")
+        {
+            DragDrop dragginObject = currentlyHovering.GetComponent<DragDrop>();
+            dragginObject.isDragging = !dragginObject.isDragging;
+        }
+    }
+}
