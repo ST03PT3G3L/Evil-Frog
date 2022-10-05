c18b001d6954d990ed76e8677c6c1c6a287eb2ef
Daan <i464733@fhict.nl>
2022-10-05 12:06:54 +0200

    Start of new enemies

---
+++ Assets/Scripts Daan/Enemy Scripts/EnemyDisplay.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDisplay : MonoBehaviour
+{
+    public Enemy enemy;
+
+    private void Start()
+    {
+    }
+}
